08d9f9e2a20d0187e743a854d371d120d74813c6
郝 士崴 <haojiayimai@outlook.com>
2017-11-17 15:47:29 +0800

    全局变量之process

---
+++ process.js
@@ -0,0 +1,11 @@
+//输出当前目录
+console.log('当前目录：' + process.cwd());
+
+//输出当前版本
+console.log('当前版本：' + process.version);
+
+//输出内存使用情况
+console.log(process.memoryUsage());
+
+//获取进行PID
+console.log('进程号：' + process.pid);
